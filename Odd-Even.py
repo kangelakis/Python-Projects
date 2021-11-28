33522791e5bdfdce404271b51a902b5baaad701a
Kronios <k.aggelakis8@gmail.com>
2021-11-28 15:14:51 +0200

    Update Odd-Even.py
    Made plot graph more presentable

--- Odd-Even.py
+++ Odd-Even.py
@@ -7,6 +7,7 @@ while (f==True):
     gr = (input ("Choose the graph method (stem/plot): ")) #gr for graph
     x=[0] # x-cords of graph
     y=[number] # y-cords of graph
+    z=[number] #The legend indication
     steps = 0
 
     #Checks whether the user has not entered one of the two graph options and makes him choose again
@@ -30,20 +31,22 @@ while (f==True):
         steps += 1
         x.append(steps) #Adds the x-cords to the list
         y.append(number) #Adds the y-cords to the list
-
+        
         if (gr == 'stem'): #Stem graph settings
             markerline, stemlines, baseline = plt.stem(
             x, y, linefmt='gray', markerfmt='D', bottom=1.1)
             markerline.set_markerfacecolor('none')
         else: #Plot graph settings
-            plt.plot(x,y)
+            plt.plot(x,y, "gray", linewidth=0.2, alpha=0.4,marker="o", markersize=0.35,
+            markerfacecolor='black',markeredgecolor="black")
+
         plt.pause(0.15) #The delay between when each point gets added to the graph animation
+        plt.legend(z) #Indication on graph of the current number
         plt.xlabel('Steps')
         plt.ylabel('Numbers')
         plt.title(('Y =', number,'Peak =', max))
 
     print("The steps needed to reach 1 were:", steps)
-    plt.show()
 
     choice = (input ("Choose another number? (Y/n): ")) #Check whether the user wants to continue
     if (choice=='Y' or choice=='y'):
@@ -54,3 +57,13 @@ while (f==True):
         choice = (input ("Invalid option, please try again.. (Y/n): ")) #Check whether the user wants to continue
 
 input("Press any button to continue...") #Terminates the program
+
+'''
+todo : Remove the stem graph and replace it with an option that allows the user
+to either see a specific number plot graph eg 27(done as gr==plot) or see a sequence of numbers
+eg from 27-42 in the same graph(see test.py)
+
+For both cases a for loop could be used in a function 
+eg1: Given the number n - for i in range(n,n): ...
+eg2: Given the numbers n,m - for i in range(n,m): ...
+'''
